a4f66f665882a91af475368f0b44c88161e0d13a
PalthiMalleswari <palthimalleswari@gmail.com>
2026-01-14 18:21:17 +0530

    Create Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,23 @@
+# Problem - https://leetcode.com/problems/binary-search/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        n = len(nums)
+        l,r = 0,n-1
+
+        while l <= r:
+
+            mid = (l+r)//2
+
+            if nums[mid]==target:
+                return mid
+            elif nums[mid] > target:
+                r = mid-1
+            else:
+                l = mid+1
+        
+        return -1
+
+Time Complexity - O(N log N)
+Space Complexity - O(1)
