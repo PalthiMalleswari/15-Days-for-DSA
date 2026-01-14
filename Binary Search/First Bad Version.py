3403bc07c6e4acc313e76d567f5d6bde0a369e71
PalthiMalleswari <palthimalleswari@gmail.com>
2026-01-14 18:44:53 +0530

    Create First Bad Version.py

---
+++ Binary Search/First Bad Version.py
@@ -0,0 +1,35 @@
+# Problem - https://leetcode.com/problems/first-bad-version/description/
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l,r = 1,n
+        ans = -1
+        while l<=r:
+            mid = (l+r)//2
+
+            if isBadVersion(mid):
+                ans = mid
+                r = mid-1
+            else:
+                l = mid+1
+        return ans
+
+Time Complexity - O(NlogN)
+Space Complexity - O(1)
+
+
+# ============ Other Way ! =================
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l,r = 1,n
+        while l<r:
+            mid = (l+r)//2
+
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid+1
+        return l
+Time Complexity - O(NlogN)
+Space Complexity - O(1)
